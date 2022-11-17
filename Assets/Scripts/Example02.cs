9663460f9b4a83a1474aabaaea8f5425f6f99d0a
dynms1337 <arch.bondage@aol.com>
2022-11-17 18:08:55 +0900

    Ex2　微調整

--- Assets/Scripts/Example02.cs
+++ Assets/Scripts/Example02.cs
@@ -27,7 +27,7 @@ public class Example02 : MonoBehaviour
 
         SWindow window = new SWindow();
         window.init_onScreen("Example02 - Components", leftbottom: new Vector2(420, 20), windowSize: new Vector2(400, 800), hooterLayout: LayoutType.Vertical);
-        
+
         //SPanel header = window.header;	//title引数を指定すると、headerには自動的にウィンドウのタイトルが記入されるので操作の必要なし
         //SPanel caption = window.caption;  //windowsで、右上の最小化ボタンなどをまとめてキャプションボタンと呼ぶことから。
         //SPanel content = window.content;
@@ -40,74 +40,56 @@ public class Example02 : MonoBehaviour
         // ************************ content ************************
         SPanel content = window.content;
 
-        UIInfo uiTitle = UIInfo.TEXT_DEFAULT.textSize(15);
+        UIInfo uiTitle = UIInfo.TEXT_DEFAULT.textSize(18);
         {
-            content.addText("TextBox", uiTitle);
-            //content.addText("TextBox");
-            string initialText = "entered string: ";
-            Text text1 = content.addText(initialText);
-            content.addTextField(onEndEdit: s => text1.text += "\r\n---\r\n\r\n", onValueChanged: s => text1.text = $"{initialText}{s}");
+            content.addText("InputField (Single line)", uiTitle);
+            string initialText = "entered string: \r\n";
+            Text log = content.addText(initialText);
+            content.addTextField(onEndEdit: s => log.text += "\r\n---\r\n\r\n", onValueChanged: s => log.text = $"{initialText}{s}\r\n");
             content.addSpacer();
         }
 
         {
-            content.addText("N行のテキストボックスの変更・確定検知の例");
-            SPanel content_textarea = content.addPanel_Horizontal();
-            Text text2 = content_textarea.addText("input text...");
-            content_textarea.addTextField(onEndEdit: s => text2.text += "entered", onValueChanged: s => text2.text = "text area :" + s);
+            content.addText("InputField (Multi lines)", uiTitle);
+            string initialText = "entered string: \r\n";
+            Text log = content.addText(initialText);
+            content.addTextField(onEndEdit: s => log.text += "\r\n---\r\n\r\n", onValueChanged: s => log.text = $"{initialText}{s}\r\n", lineCount: 2);
+            content.addSpacer();
         }
-        content.addText("トグルボタンのbool取得の例");
-        SPanel content_toggle = content.addPanel_Horizontal();
-        Text text3 = content_toggle.addText("click toggle...");
-        content_toggle.addToggle(b => text3.text = "Toggle Status :" + (b ? "ON" : "OFF"), "switching", isOn: false);
+        {
+            content.addText("Button", uiTitle);
+            Text log = content.addText("click count : 0");
+            int clickCount = 0;
+            content.addButton(() =>
+            {
+                clickCount++;
+                log.text = $"click count : {clickCount}";
+            }
+            , "click here");
+            content.addSpacer();
+        }
+        {
+            content.addText("Toggle", uiTitle);
+            SPanel content_toggle = content.addPanel_Horizontal();
+            Text textToggleStatus = content_toggle.addText("Toggle Status...");
+            content_toggle.addToggle(b => textToggleStatus.text = "Toggle Status :" + (b ? "ON" : "OFF"), "switch here!", isOn: false);
+            content.addSpacer();
+        }
 
-        content.addText("ラジオボタンから選択アイテム取得の例");
-        SPanel content_radio = content.addPanel_Horizontal();
-        Text text_radio = content_radio.addText("click radio button...");
-        content_radio.addRadioButton(s => text_radio.text = "selected :" + s, itemList[0], layoutGroup: LayoutType.Horizontal);
-
-        content.addText("スライダからfloat取得の例");
-        SPanel content_slider = content.addPanel_Horizontal();
-        Text text_slider = content_slider.addText("drag slider...");
-        content_slider.addSlider(f => text_slider.text = "value :" + f.ToString());
-
-
-
-        //foreach (var d in itemList)
-        //{
-        //    SPanel vp = content.addPanel_Vertical(uiInfo:UIInfo.PANEL_DEFAULT.bgColor(new Color(1f, 1f, 1f, 0.1f)));
-        //    foreach (var kvp in d)
-        //    {
-        //        SPanel hp = vp.addPanel_Horizontal();
-        //        hp.addText(kvp.Key);
-        //        hp.addText(kvp.Value);
-        //    }
-        //}
-
-        //hooter.addText("１行のテキストボックスの例");
-        //SPanel hooter_textfield = hooter.addPanel_Horizontal();
-        //Text text1 = hooter_textfield.addText("input text in text field...");
-        //hooter_textfield.addTextField(onEndEdit: s => text1.text += "entered", onValueChanged: s => text1.text = "text field :" + s);
-
-        //hooter.addText("N行のテキストボックスの変更・確定検知の例");
-        //SPanel hooter_textarea = hooter.addPanel_Horizontal();
-        //Text text2 = hooter_textarea.addText("input text...");
-        //hooter_textarea.addTextField(onEndEdit: s => text2.text += "entered", onValueChanged: s => text2.text = "text area :" + s);
-
-        //hooter.addText("トグルボタンのbool取得の例");
-        //SPanel hooter_toggle = hooter.addPanel_Horizontal();
-        //Text text3 = hooter_toggle.addText("click toggle...");
-        //hooter_toggle.addToggle(b => text3.text = "Toggle Status :" + (b ? "ON" : "OFF"), "switching", isOn: false);
-
-        //hooter.addText("ラジオボタンから選択アイテム取得の例");
-        //SPanel hooter_radio = hooter.addPanel_Horizontal();
-        //Text text_radio = hooter_radio.addText("click radio button...");
-        //hooter_radio.addRadioButton(s => text_radio.text = "selected :" + s, itemList[0], layoutGroup: LayoutType.Horizontal);
-
-        //hooter.addText("スライダからfloat取得の例");
-        //SPanel hooter_slider = hooter.addPanel_Horizontal();
-        //Text text_slider = hooter_slider.addText("drag slider...");
-        //hooter_slider.addSlider(f => text_slider.text = "value :" + f.ToString());
+        {
+            content.addText("Radio Buttons (by ToggleGroup)", uiTitle);
+            SPanel content_radio = content.addPanel_Horizontal();
+            Text text_radio = content_radio.addText("click radio button...");
+            content_radio.addRadioButton(s => text_radio.text = "selected :" + s, itemList[0], layoutGroup: LayoutType.Horizontal);
+            content.addSpacer();
+        }
+        {
+            content.addText("Slider", uiTitle);
+            SPanel content_slider = content.addPanel_Horizontal();
+            Text text_slider = content_slider.addText("drag slider...");
+            content_slider.addSlider(f => text_slider.text = "value :" + f.ToString());
+            content.addSpacer();
+        }
     }
 
 
